658306cdddb32e42509d3c77babb910090fc71f3
heerim0223 <stormsky3@naver.com>
2026-07-12 21:12:38 +0900

    feat: 시작 화면, 유저 메뉴 구현

--- script/CharacterMove.gd
+++ script/CharacterMove.gd
@@ -1,15 +1,32 @@
 extends CharacterBody2D
 
-# 캐릭터 이동 속도 상수
-const SPEED = 200.0 
+const SPEED = 200.0
 
 @onready var anim = $AnimatedSprite2D
 
+var menu_scene = preload("res://scene/menu/game_menu.tscn")
+var menu
+
+func _ready():
+	menu = menu_scene.instantiate()
+	menu.visible = false
+	get_tree().current_scene.call_deferred("add_child", menu)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("open_menu"):
+		menu.visible = !menu.visible
+
 func _physics_process(delta):
+	# 메뉴가 열려 있으면 플레이어 이동 금지
+	if menu.visible:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+
 	var direction = Vector2.ZERO
 
 	z_index = int(global_position.y + 16)
-	
+
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 	elif Input.is_action_pressed("ui_left"):
@@ -23,7 +40,6 @@ func _physics_process(delta):
 	velocity = direction.normalized() * SPEED
 	move_and_slide()
 
-	# 애니메이션
 	if direction == Vector2.ZERO:
 		if anim.animation == "move_front":
 			anim.play("stop_front")
