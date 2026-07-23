5d0d4f6aee3d3fddb951594765020553547ff8fe
kingluminance <kingluminance@gmail.com>
2026-07-24 06:22:45 +0900

    [#3] feat : 간단한 테스트 맵 제작 및 캐릭 토글시 비활성화 로직 추가

--- scripts/world/sub_map_schizophrenia/Schizophrenia.gd
+++ scripts/world/sub_map_schizophrenia/Schizophrenia.gd
@@ -6,7 +6,8 @@ var current_mode = ViewMode.BOTH
 
 @onready var container_a = $UILayer/ContainerA
 @onready var container_b = $UILayer/ContainerB
-
+@onready var player_a = $UILayer/ContainerA/ViewportA/WorldA/Player
+@onready var player_b = $UILayer/ContainerB/ViewportB/WorldB/Player
 # 시점 변경
 func set_view_mode(mode: ViewMode):
 	current_mode = mode
@@ -15,15 +16,21 @@ func set_view_mode(mode: ViewMode):
 			container_a.visible = true
 			container_a.modulate.a = 1.0
 			container_b.visible = false
+			_set_player_active(player_a, true)
+			_set_player_active(player_b, false)
 		ViewMode.B_ONLY:
 			container_a.visible = false
 			container_b.visible = true
 			container_b.modulate.a = 1.0
+			_set_player_active(player_a, false)
+			_set_player_active(player_b, true)
 		ViewMode.BOTH:
 			container_a.visible = true
 			container_b.visible = true
 			container_a.modulate.a = 1.0
 			container_b.modulate.a = 0.6  # 겹침 효과
+			_set_player_active(player_a, true)
+			_set_player_active(player_b, true)
 	_update_dialogue(mode)
 	
 func _input(event):
@@ -38,6 +45,11 @@ func _cycle_view_mode():
 			set_view_mode(ViewMode.BOTH)
 		ViewMode.BOTH:
 			set_view_mode(ViewMode.A_ONLY)
+			
+# 플레이어 껏다 키기
+func _set_player_active(player: Node, active: bool):
+	player.process_mode = Node.PROCESS_MODE_INHERIT if active else Node.PROCESS_MODE_DISABLED
+
 
 func _update_dialogue(mode: ViewMode):
 	match mode:
